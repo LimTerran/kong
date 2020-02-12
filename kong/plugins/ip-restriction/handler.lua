04be3373025c60a6b3534919d262ca1dfe7cf346
Vinicius Mignot <vinicius.mignot@gmail.com>
2020-02-12 14:12:50 -0300

    feat(plugins) plugins errors using new PDK method

--- kong/plugins/ip-restriction/handler.lua
+++ kong/plugins/ip-restriction/handler.lua
@@ -37,7 +37,7 @@ function IpRestrictionHandler:access(conf)
   local binary_remote_addr = ngx.var.binary_remote_addr
 
   if not binary_remote_addr then
-    return kong.response.exit(FORBIDDEN, { message = "Cannot identify the client IP address, unix domain sockets are not supported." })
+    return kong.response.error(FORBIDDEN, "Cannot identify the client IP address, unix domain sockets are not supported.")
   end
 
   if conf.blacklist and #conf.blacklist > 0 then
